ebc8b42bf2d3c2bffc3c4be9615170e2d385342d
Seunghoon Lee <seunghoonlee95@gmail.com>
2021-06-16 00:07:07 +0900

    JS: project filtering

--- main.js
+++ main.js
@@ -23,6 +23,31 @@ navbarMenu.addEventListener('click', (event) =>{
   scrollIntoView(link);
 });
 
+//Project filtering
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+
+workBtnContainer.addEventListener('click', (e)=>{
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if(filter == null){
+    return;
+  }
+
+  projectContainer.classList.add('anim-out');
+
+  setTimeout(() => {
+    projects.forEach((project) => {
+      if(filter === '*' || filter === project.dataset.type){
+        project.classList.remove('invisible');
+      }else{
+        project.classList.add('invisible');
+      }
+    });
+    projectContainer.classList.remove('anim-out');
+  }, 300);
+});
+
 //Handle scrolling when tapping on the 'home__contact' button
 const contactButton = document.querySelector('.home__contact');
 
