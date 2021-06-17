ac2798505b896ae7addf98b9d88ca8bb67a1f7ac
Seunghoon Lee <seunghoonlee95@gmail.com>
2021-06-17 15:47:04 +0900

    Final touch

--- main.js
+++ main.js
@@ -35,16 +35,18 @@ navbarToggleBtn.addEventListener('click', ()=>{
 //Project filtering
 const workBtnContainer = document.querySelector('.work__categories');
 const projectContainer = document.querySelector('.work__projects');
-const projects = document.querySelectorAll('.project');
+const projects = document.querySelectorAll('.project'); //project들이 담긴 배열이 리턴됨.
 
 workBtnContainer.addEventListener('click', (e)=>{
+  //category__count(span임)를 클릭하는경우 data-set에 아무 값도 없으므로, 
+  //그span의 부모(button) 이 선택되도록 아래에 두번째 경우를 설정
   const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
   if(filter == null){
     return;
   }
 
   //Remove selection from the previous item and select the new one.
-  const active = document.querySelector('.category__btn.selected');
+  const active = document.querySelector('.category__btn.selected');  
   active.classList.remove('selected');
   console.log(`e.target.nodeName : ${e.target.nodeName}`);
   const target = e.target.nodeName ==='BUTTON' ? e.target : e.target.parentNode;
@@ -83,7 +85,7 @@ arrowUpButton.addEventListener('click', ()=>{
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 
-document.addEventListener('scroll', () =>{
+document.addEventListener('scroll', () =>{  
   home.style.opacity = 1 - window.scrollY / homeHeight;
 
   //Show "arrow up" button when scrolling down
